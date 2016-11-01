9af883821109c6f6e6aa1ae7bee4310ce9abda14
RoberJames <rjames@northpointdigital.com>
2016-11-01 17:25:51 -0400

    small edit

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
-'use strict';
+const express = require('express');
+const app = express();
 
-var express = require('express');
-var app = express();
+// app.use((req,res, next) => {
+// 	console.log(req.headers)
+// 	next()
+// })
 
-app.get('/', function(req,res) {
-	res.send('Hey World!!');
-});
+// app.use((req,res, next) => {
+// 	req.chance = Math.random()
+// 	next()
+// })
 
-app.listen(5000);
+// app.use((err,req,res, next) => {
+// 	console.log(err)
+// 	res.status(500).send('something happended')
+// })
+
+app.get('/', (req,res) => {
+	res.send('Hey World!!')
+	})
+
+app.listen(3000, (err) => {
+	if(err) {
+		return console.log('something happened',err)
+	}
+	console.log('Server Listening on 3000')
+})
+
+
+
+
+// const express = require('express')
+// const app = express()
+
+
+// app.get('/', (req,res) => {
+// 	throw new Error('my bad')
+// })
+
+// app.use((err,req,res, next) => {
+// 	console.log(err)
+// 	res.status(500).send('something happended')
+// })
+
+// app.listen(3000, (err) => {
+// 	if(err) {
+// 		return console.log('something happened',err)
+// 	}
+// 	console.log('Server Listening on 3000')
+// })
