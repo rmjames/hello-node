e5c276506ea3351dc7ea7087652d9625283e3c15
Robert James <rma.james@gmail.com>
2017-10-23 13:28:17 -0400

    removed comments

--- app.js
+++ app.js
@@ -1,21 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.use((req,res, next) => {
-// 	console.log(req.headers)
-// 	next()
-// })
-
-// app.use((req,res, next) => {
-// 	req.chance = Math.random()
-// 	next()
-// })
-
-// app.use((err,req,res, next) => {
-// 	console.log(err)
-// 	res.status(500).send('something happended')
-// })
-
 app.get('/', (req,res) => {
 	res.send('Hey World!!')
 	})
@@ -26,26 +11,3 @@ app.listen(3000, (err) => {
 	}
 	console.log('Server Listening on 3000')
 })
-
-
-
-
-// const express = require('express')
-// const app = express()
-
-
-// app.get('/', (req,res) => {
-// 	throw new Error('my bad')
-// })
-
-// app.use((err,req,res, next) => {
-// 	console.log(err)
-// 	res.status(500).send('something happended')
-// })
-
-// app.listen(3000, (err) => {
-// 	if(err) {
-// 		return console.log('something happened',err)
-// 	}
-// 	console.log('Server Listening on 3000')
-// })
